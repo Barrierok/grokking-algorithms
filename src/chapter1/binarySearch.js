90cb5c44909daef3e2c898a757bf7096b4123f0b
IlyaZubtsov <ilyazubtsov@noorsoft.ru>
2020-07-25 09:49:59 +0300

    refactoring binary search

--- src/chapter1/binarySearch.js
+++ src/chapter1/binarySearch.js
@@ -5,15 +5,16 @@ export const binarySearch = (list, value) => {
   while (low <= high) {
     const mid = Math.ceil((low + high) / 2);
     const guess = list[mid];
-    if (guess === value) {
-      return mid;
-    }
     if (guess < value) {
       low = mid + 1;
+      break;
     }
     if (guess > value) {
       high = mid - 1;
+      break;
     }
+    return mid;
   }
+
   return -1;
 };
